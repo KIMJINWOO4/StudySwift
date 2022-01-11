aed1c4d51c6560661b76ad8a43ce9a8341cc5e8b
Jinwoo Kim <80628077+KIMJINWOO4@users.noreply.github.com>
2022-01-11 15:34:04 +0900

    a
    a

--- storyboardNOLI/ViewController.swift
+++ storyboardNOLI/ViewController.swift
@@ -8,12 +8,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var num1: UILabel!
+    @IBOutlet var num2: UILabel!
+    @IBOutlet var userAns: UITextField!
+    @IBOutlet var wrong: UILabel!
+    @IBOutlet var correct: UILabel!
+    @IBOutlet var count: UILabel!
+    @IBOutlet var sB: UIButton!
+    
+    var corr: Int = 0
+    var wron: Int = 0
+    var cnt: Int = 0
+    var n1: Int = 0
+    var n2: Int = 0
+    var nu: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func start(){
+        n1 = Int(arc4random() % 12) + 1
+        n2 = Int(arc4random() % 12) + 1
+        num1.text = "\(n1)"
+        num2.text = "\(n2)"
+        sB.isEnabled = false
+    }
+    @IBAction func curcul(){
+        nu = Int(userAns.text!)!
+        let realAns: Int = Int(n1 * n2)
+        if nu == realAns {
+            corr += 1
+            correct.text = "\(corr)"
+            cnt += 1
+            count.text = "\(cnt)"
+            userAns.text = ""
+            n1 = Int(arc4random() % 12) + 1
+            n2 = Int(arc4random() % 12) + 1
+            num1.text = "\(n1)"
+            num2.text = "\(n2)"
+            
+        }
+        else {
+            wron += 1
+            wrong.text = "\(wron)"
+            cnt += 1
+            count.text = "\(cnt)"
+            userAns.text = ""
+            n1 = Int(arc4random() % 12) + 1
+            n2 = Int(arc4random() % 12) + 1
+            num1.text = "\(n1)"
+            num2.text = "\(n2)"
+        }
+    }
+    @IBAction func stopClick(){
+        corr = 0
+        cnt = 0
+        n1 = 0
+        n2 = 0
+        nu = 0
+        wron = 0
+        correct.text = "0"
+        wrong.text = "0"
+        count.text = "0"
+        num1.text = "0"
+        num2.text = "0"
+        userAns.text = ""
+        sB.isEnabled = true
+    }
 }
 
