1fcf1eb72e9b921cfd4934fc31def245486e87c5
Jinwoo Kim <80628077+KIMJINWOO4@users.noreply.github.com>
2022-01-31 20:12:25 +0900

    typecasting

---
+++ StudySwift/swift_tc.playground/Contents.swift
@@ -0,0 +1,66 @@
+class Person {
+    var name: String = ""
+    func breath() {
+        print("숨을 쉽니다")
+    }
+}
+
+class Student: Person {
+    var school: String = ""
+    func goToSchool() {
+        print("등교를 합니다")
+    }
+}
+
+class UniversityStudent: Student {
+    var major: String = ""
+    func goToMT() {
+        print("멤버쉽 트레이닝을 갑니다 신남!")
+    }
+}
+var yagom: Person = Person()
+var hana: Student = Student()
+var jason: UniversityStudent = UniversityStudent()
+var result: Bool
+
+result = yagom is Person // true
+result = yagom is Student // false
+result = yagom is UniversityStudent // false
+
+result = hana is Person // true
+result = hana is Student // true
+result = hana is UniversityStudent // false
+
+result = jason is Person // true
+result = jason is Student // true
+result = jason is UniversityStudent // true
+
+if yagom is UniversityStudent {
+    print("yagom은 대학생입니다")
+} else if yagom is Student {
+    print("yagom은 학생입니다")
+} else if yagom is Person {
+    print("yagom은 사람입니다")
+} // yagom은 사람입니다
+
+switch jason {
+case is Person:
+    print("jason은 사람입니다")
+case is Student:
+    print("jason은 학생입니다")
+case is UniversityStudent:
+    print("jason은 대학생입니다")
+default:
+    print("jason은 사람도, 학생도, 대학생도 아닙니다")
+} // jason은 사람입니다
+
+switch jason {
+case is UniversityStudent:
+    print("jason은 대학생입니다")
+case is Student:
+    print("jason은 학생입니다")
+case is Person:
+    print("jason은 사람입니다")
+default:
+    print("jason은 사람도, 학생도, 대학생도 아닙니다")
+} // jason은 대학생입니다
