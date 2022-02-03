837fb62a17811a6a5fde93e208956315b9096165
Jinwoo Kim <80628077+KIMJINWOO4@users.noreply.github.com>
2022-02-03 16:12:59 +0900

    v

---
+++ StudySwift/vari_ex.playground/Contents.swift
@@ -0,0 +1,19 @@
+var someBool: Bool = true
+someBool = false
+var someInt: Int = -100
+var someUInt: UInt = 100
+var someFloat: Float = 3.14
+someFloat = 3
+var someDouble: Double = 3.14
+someDouble = 3
+var someCharacter: Character = "🇰🇷"
+someCharacter = "😄"
+someCharacter = "가"
+someCharacter = "A"
+print(someCharacter)
+var someString: String = "하하하 😄 "
+someString = someString + "웃으면 복이와요"
+print(someString)
+let integer = 100
+let floatingPoint = 12.34
+let apple = "A"
